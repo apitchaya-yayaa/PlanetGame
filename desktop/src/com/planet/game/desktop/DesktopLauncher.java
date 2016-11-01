6a202fe63b7afa8236854add7738d43e183b0160
YAYAA <YAYAA@192.168.10.107>
2016-11-01 18:38:01 +0700

    first commit

---
+++ desktop/src/com/planet/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.planet.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.planet.game.PlanetGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PlanetGame(), config);
+	}
+}
